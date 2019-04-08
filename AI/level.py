ec715997333a91a712e104d769ec9939f8ce0ba8
lagxia <maxou-910516@hotmail.fr>
2019-04-08 14:55:29 +0200

    Class level
    Add class level

---
+++ AI/level.py
@@ -0,0 +1,19 @@
+class Level:
+    def __init__(self):
+        print("Level")
+
+    def difficultySelect(self):
+        print("select difficulty")
+
+    def __easyMod__(self):
+        print("Easy")
+
+    def __mediumMod__(self):
+        print("Medium")
+
+    def __hardMod__(self):
+        print("Hard")
+
+    def __godMod__(self):
+        print("God")
+
