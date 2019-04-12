4c59c02123ff11da194f874168f9506433575fee
lagxia <maxou-910516@hotmail.fr>
2019-04-12 16:59:09 +0200

    Create Grid UI
    Error to create Line

--- Interface/plateau.py
+++ Interface/plateau.py
@@ -1,3 +1,27 @@
-class Plateau:
+from tkinter import Tk, Canvas, Frame, BOTH
+
+class Plateau(Frame):
+
+    def DrawGrid(self):
+
+        # draw horizontal lines
+        x1 = 0
+        x2 = 450
+        for k in range(0, 500, 50):
+            y1 = k
+            y2 = k
+            Canvas.create_line(x1, y1, x2, y2)
+        # draw vertical lines
+        y1 = 0
+        y2 = 450
+        for k in range(0, 500, 50):
+            x1 = k
+            x2 = k
+            Canvas.create_line(x1, y1, x2, y2)
+
     def __init__(self):
-        print("plateau")
+        super().__init__()
+        self.DrawGrid()
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello-Word")
+import Interface.plateau
+
+from tkinter import Tk, Canvas, Frame, BOTH
+
+def main():
+    root = Tk()
+    canvas = Tk.Canvas(root, width=450, height=450, bg='white')
+    canvas.pack()
+    ex = Interface.plateau.Plateau()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
